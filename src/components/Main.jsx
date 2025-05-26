8efc40de1390d6fb637a9d6f796093d755b1b5a8
anurag-sharma-pendyala <pendyalaanuragsharma@gmail.com>
2025-05-25 20:20:02 -0400

    Added new Sections
    Added Hero,Skills,Projects & About components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+
+import Contact from './Contact'
+import Hero from './Hero'
+import Projects from './Projects'
+import Skills from './Skills'
 
 const Main = () => {
   return (
-    <main className='main container'>Main</main>
+    <main className='main container'>
+       <Hero/>
+       <Projects/>
+       <Skills/>
+       <Contact/>
+    </main>
   )
 }
 
