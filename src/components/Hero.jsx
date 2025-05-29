4e3197f9542211f4591b0bc80e90bbad7332fda5
anurag-sharma-pendyala <pendyalaanuragsharma@gmail.com>
2025-05-28 20:27:36 -0400

    Started Writing the Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,25 @@
+import Button from "./Button"
 
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="hero-section"> 
+      <div className="container hero-wrapper flex">
+        <div className="hero-content">
+        <h1 className="hero-title">Hi! This is Anurag Sharma Pendyala</h1>
+        <p className="hero-description">I am Experienced Web Developer, Data Enthusiast</p>
+        <div className="button-wrapper flex">
+           <Button name={"View Projects"} className="action-button"/>
+        <Button name={"Download Resume"} className="inactive-button"/>
+        </div>
+       
+        </div>
+        <div className="hero-image-wrapper">
+          <img src="" alt="Here we need include the image" />
+        </div>
+      </div>
+
+    </section>
   )
 }
 
